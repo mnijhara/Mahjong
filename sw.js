c9e3af407f3cce8d2ea89e604b35b3a25dbcc7b0
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-09-20 16:42:00 +0530

    chore: refresh PWA shell for accessibility styles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mahjong-static-v16';
+const CACHE_NAME = 'mahjong-static-v17';
 const APP_SHELL = [
   './',
   './index.html',
@@ -35,13 +35,15 @@ const APP_SHELL = [
   './solitaire-fit.js?v=20260830-1',
   './american-ux.js?v=20260830-7',
   './american-tile-polish.js?v=20260905-1',
-  './american-table-polish.js?v=20260905-1',
+  './american-table-polish.css',
+  './american-table-polish.js',
   './mahjong-codex.js',
   './sw-register.js',
   './manifest.webmanifest',
   './icons/mahjong-192.svg',
   './icons/mahjong-512.svg',
-  './robots.txt'
+  './robots.txt',
+  './accessibility.css'
 ];
 
 self.addEventListener('install', event => {
